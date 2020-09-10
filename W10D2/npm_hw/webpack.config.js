20926310415002dc753729345b5e2d667e9bb805
johnhcody <john.haner.cody@gmail.com>
2020-09-10 20:38:31 +0700

    HW commit

---
+++ W10D2/npm_hw/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./app.jsx",
+    output: {
+        filename: './bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
---
+++ W10D4/skeleton 8/frontend/reducer.js
@@ -0,0 +1,22 @@
+import { SWITCH_LOCATION } from "./actions";
+
+const initialState = {
+  city: "Please Select", 
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SWITCH_LOCATION:
+      return  {
+        jobs: action.jobs,
+        city: action.city
+      }
+    default:
+      return state;
+  }
+};
+
+window.reducer = reducer;
+
+export default reducer;
